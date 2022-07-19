c1f9857e241ce276ed49fbf461709fee7d522f41
seblemariam <Seblemariam.sm@gmail.com>
2022-07-19 12:27:26 -0700

    Sorting

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort-write a function that sort an array using bubble
+ * @array:after each time you swa two element
+ * @size:lenght of array
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+}
